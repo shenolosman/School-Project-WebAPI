f4033183abba4fb26401ab5573e453522b675724
Shenol Osman <7887878+shenolosman@users.noreply.github.com>
2022-04-21 10:55:34 +0200

    DI in ny controller

---
+++ GeoComment/Controllers/GeoController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using GeoComment.Services;
+
+namespace GeoComment.Controllers
+{
+    [Route("api/geo-comments")]
+    
+    [ApiController]
+    public class GeoController : ControllerBase
+    {
+        private readonly GeoService _geoService;
+        public GeoController(GeoService geoService)
+        {
+            _geoService = geoService;
+        }
+
+    }
+}
---
+++ GeoComment/Services/GeoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GeoComment.Data;
+
+namespace GeoComment.Services
+{
+    public class GeoService
+    {
+        private readonly GeoDbContext _dbContex;
+
+        public GeoService(GeoDbContext dbContex)
+        {
+            _dbContex = dbContex;
+        }
+    }
+}
